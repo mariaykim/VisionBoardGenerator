99c9bc36cee37c991622fc51a7b6cf1924c2ba68
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-04-08 15:17:51 -0400

    create tabs

---
+++ VisionBoardGenerator/HomeView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  VisionBoardGenerator
+//
+//  Created by Maria Kim on 4/8/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @ObservedObject var viewModel = HomeViewViewModel()
+    @State var text = String()
+    @State var image: UIImage?
+    
+    var body: some View {
+            VStack {
+                Spacer()
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                } else {
+                    Text("Type prompt to generate image")
+                }
+                Spacer()
+                TextField("Type prompt here...", text: $text)
+                    .padding()
+                Button("Generate!") {
+                    if !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty { // saving on credits
+                        Task {
+                            await generateImage(prompt: text)
+                        }
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.setup()
+            }
+            .padding()
+    }
+    
+    func generateImage(prompt: String) async {
+        let result = await viewModel.generateImage(prompt: text)
+        if result == nil {
+            print("Failed to retrieve image")
+        }
+        self.image = result
+    }
+}
+
+#Preview {
+    HomeView()
+}
