22db5da09fd7defc2aea1abbb09da62929cfdeaa
Maria Kim <80354714+mariaykim@users.noreply.github.com>
2024-04-08 13:11:03 -0400

    Create VisionBoardGeneratorApp.swift

---
+++ VisionBoardGenerator/VisionBoardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardGeneratorApp.swift
+//  VisionBoardGenerator
+//
+//  Created by Maria Kim on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
